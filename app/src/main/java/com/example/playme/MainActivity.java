89db9f1606ec33e303cae94957ad4b5d1c9be280
Prakhar-Rathore <pralhar2000@gmail.com>
2020-10-18 11:47:47 +0530

    Minor Changes

--- app/src/main/java/com/example/playme/MainActivity.java
+++ app/src/main/java/com/example/playme/MainActivity.java
@@ -58,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
